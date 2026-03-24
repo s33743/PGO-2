f2498c45637d023e3cd0dd43a5533da098732ed2
s33743 <s33743@pjwstk.edu.pl>
2026-03-24 21:00:26 +0100

    Zadanie 2 dokończone

---
+++ MiniKlasa/src/Biblioteka.java
@@ -0,0 +1,15 @@
+public class Biblioteka {
+    private Ksiazka[] ksiazki;
+    private int liczbaKsiazek;
+
+    public Biblioteka(int pojemnosc) {
+    ksiazki = new Ksiazka[pojemnosc];
+    liczbaKsiazek = 0;
+    }
+
+    public void dodajKsiazke(Ksiazka ksiazka) {
+        if (liczbaKsiazek < ksiazki.length) {
+
+        }
+    }
+}
